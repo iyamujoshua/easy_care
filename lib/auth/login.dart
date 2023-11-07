d9948de3bccc6b311eb55b050ccfa9ef4ec8fa4c
iyamujoshua <joshamu20@gmail.com>
2023-11-07 14:20:13 +0100

    Finished Login Screen

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -61,139 +61,231 @@ class _LoginState extends State<Login> {
       body: Container(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 10,
-              ),
-              TextFormField1(
-                hintText: '',
-                prefixIcon: SvgPicture.asset(
-                  "asset/Email.svg",
-                  width: 5,
-                  height: 5,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 10,
                 ),
-                label: 'example@email.com',
-                suffixIcon: null,
-                obscureText: false,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField1(
-                hintText: '',
-                prefixIcon: SvgPicture.asset(
-                  "asset/Password.svg",
-                  width: 5,
-                  height: 5,
-                ),
-                label: 'Enter password',
-                suffixIcon: IconButton(
-                  icon: isPasswordVisible
-                      ? const Icon(Icons.visibility_off,
-                          color: Color(0xFF292D32))
-                      : const Icon(Icons.visibility, color: Color(0xFF292D32)),
-                  onPressed: () =>
-                      setState(() => isPasswordVisible = !isPasswordVisible),
-                ),
-                obscureText: isPasswordVisible,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      // '/ForgetPassword'
-                      Navigator.pushNamed(context, '/ForgetPassword');
-                    },
-                    child: Center(
-                        child: Text(
-                      "Forgot password?",
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontSize: 15,
-                        color: BrandColors.colorbackground,
-                      ),
-                    )),
+                TextFormField1(
+                  hintText: '',
+                  prefixIcon: SvgPicture.asset(
+                    "asset/Email.svg",
+                    width: 5,
+                    height: 5,
                   ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomButtonWithBorder(
-                text: "Login",
-                onPressed: () {
-                  Navigator.pushNamed(context, '/HomePage');
-                },
-                borderColor:
-                    Colors.transparent, // Customize the border color here
-                borderWidth: 1.0, // Customize the border width here
-                buttonColor: BrandColors
-                    .colorbackground, // Customize the background color here
-                textStyle: TextStyle(
-                  color: Colors.white, // Customize the text color here
-                  fontSize: 14, // Customize the font size here
-                  fontWeight: FontWeight.bold, // Customize the font weight here
+                  label: 'example@email.com',
+                  suffixIcon: null,
+                  obscureText: false,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 21),
-                    child: Text(
-                      "Don't have an account? ",
-                    ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField1(
+                  hintText: '',
+                  prefixIcon: SvgPicture.asset(
+                    "asset/Password.svg",
+                    width: 5,
+                    height: 5,
                   ),
-                  InkWell(
-                      onTap: (() {
-                        Navigator.pushNamed(context, '/signup');
-                      }),
-                      child: Text(
-                        "Sign up,",
-                        style: TextStyle(color: BrandColors.colorbackground),
-                      )),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 370,
-                child: ElevatedButton(
-                  style: style,
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 15),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Image.asset(
-                          "asset/Google.png",
-                          width: 30,
-                          height: 30,
+                  label: 'Enter password',
+                  suffixIcon: IconButton(
+                    icon: isPasswordVisible
+                        ? const Icon(Icons.visibility_off,
+                            color: Color(0xFF292D32))
+                        : const Icon(Icons.visibility,
+                            color: Color(0xFF292D32)),
+                    onPressed: () =>
+                        setState(() => isPasswordVisible = !isPasswordVisible),
+                  ),
+                  obscureText: isPasswordVisible,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        // '/ForgetPassword'
+                        Navigator.pushNamed(context, '/ForgetPassword');
+                      },
+                      child: Center(
+                          child: Text(
+                        "Forgot password?",
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 15,
+                          color: BrandColors.colorbackground,
                         ),
-                        SizedBox(width: 8),
-                        Text('Sign in with Google',
-                            style:
-                                TextStyle(fontSize: 17, color: Colors.black)),
-                        Text("         "),
-                        Text("")
-                      ],
+                      )),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                CustomButtonWithBorder(
+                  text: "Login",
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/HomePage');
+                  },
+                  borderColor:
+                      Colors.transparent, // Customize the border color here
+                  borderWidth: 1.0, // Customize the border width here
+                  buttonColor: BrandColors
+                      .colorbackground, // Customize the background color here
+                  textStyle: TextStyle(
+                    color: Colors.white, // Customize the text color here
+                    fontSize: 14, // Customize the font size here
+                    fontWeight:
+                        FontWeight.bold, // Customize the font weight here
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 21),
+                      child: Text(
+                        "Don't have an account? ",
+                      ),
+                    ),
+                    InkWell(
+                        onTap: (() {
+                          Navigator.pushNamed(context, '/signup');
+                        }),
+                        child: Text(
+                          "Sign up,",
+                          style: TextStyle(color: BrandColors.colorbackground),
+                        )),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey,
+                        height: 2,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Container(
+                        decoration: BoxDecoration(color: Colors.grey),
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text("OR"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.grey,
+                        height: 2,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 370,
+                  child: ElevatedButton(
+                    style: style,
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Image.asset(
+                            "asset/Google.png",
+                            width: 30,
+                            height: 30,
+                          ),
+                          SizedBox(width: 8),
+                          Text('Sign in with Google',
+                              style:
+                                  TextStyle(fontSize: 17, color: Colors.black)),
+                          Text("         "),
+                          Text("")
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 370,
+                  child: ElevatedButton(
+                    style: style,
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Image.asset(
+                            "asset/Facebook.png",
+                            width: 30,
+                            height: 30,
+                          ),
+                          SizedBox(width: 8),
+                          Text('Sign in with Google',
+                              style:
+                                  TextStyle(fontSize: 17, color: Colors.black)),
+                          Text("         "),
+                          Text("")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 370,
+                  child: ElevatedButton(
+                    style: style,
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Image.asset(
+                            "asset/Apple.png",
+                            width: 30,
+                            height: 30,
+                          ),
+                          SizedBox(width: 8),
+                          Text('Sign in with Google',
+                              style:
+                                  TextStyle(fontSize: 17, color: Colors.black)),
+                          Text("         "),
+                          Text("")
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
